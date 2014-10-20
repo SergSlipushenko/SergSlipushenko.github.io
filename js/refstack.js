0ac53f54b041fab3b4fd6dad67bffbb3582c963f
sslypushenko <sslypushenko@mirantis.com>
2014-10-20 19:18:38 +0300

    .

--- js/refstack.js
+++ js/refstack.js
@@ -7,46 +7,96 @@ function chart_bullets(text, render){
     }
 }
 
+function caps_support(text, render){
+    if (this.fully_supported) {
+        return render('<li class="fa fa-check-square-o passed"><span>' + text+ '</span></li>')
+    }
+    else if (this.particulary_supported) {
+        return render('<li class="fa fa-square part_passed"><span>' + text+ '</span></li>')
+    }
+    else {
+        return render('<li class="fa fa-times failed"><span>' + text+ '</span></li>')
+    }
+}
+
 function build_result(caps_list){
     var test_result = $.parseJSON($('#passed_tests').html());
-    console.log(test_result);
-    for (var cap_class in caps_list.capabilities){
-        for (var cap in caps_list.capabilities[cap_class].items){
-            var capability = caps_list.capabilities[cap_class].items[cap];
+    var other_tests = test_result.results.slice(0);
+    var result = {'defcore_tests': {
+        'capabilities': caps_list.capabilities,
+        'list': [],
+        'cpid': test_result.cpid,
+        'duration_seconds': test_result.duration_seconds
+    }};
+    for (var cap_class in result.defcore_tests.capabilities){
+        result.defcore_tests.capabilities[cap_class].full_support_count = 0;
+        result.defcore_tests.capabilities[cap_class].particular_support_count = 0;
+        for (var cap in result.defcore_tests.capabilities[cap_class].items){
+            var capability = result.defcore_tests.capabilities[cap_class].items[cap];
             capability.passed_tests = [];
             capability.failed_tests = [];
             capability.test_chart = [];
+            capability.fully_supported = true;
+            capability.particulary_supported = false;
             for (var test in capability.tests){
                 var passed = test_result.results.indexOf(capability.tests[test]) >= 0;
                 if (passed) {
+                    capability.particulary_supported = true;
                     capability.passed_tests.push(capability.tests[test]);
-                    capability.test_chart.push(1)
+                    capability.test_chart.push(1);
+                    var test_index = other_tests.indexOf(capability.tests[test]);
+                    if (test_index>=0) {
+                        other_tests.splice(test_index, 1);
+                        result.defcore_tests.list.push(capability.tests[test])
+                    }
                 }
                 else {
+                    capability.fully_supported = false;
                     capability.failed_tests.push(capability.tests[test]);
                     capability.test_chart.push(-1)
                 }
             }
+            if (capability.fully_supported) {
+                capability.particulary_supported = false
+            }
             capability.passed_count = capability.passed_tests.length;
             capability.failed_count = capability.failed_tests.length;
             capability.chart_bullets = function() {return chart_bullets};
-            caps_list.capabilities[cap_class].items[cap] = capability
+            capability.caps_support = function() {return caps_support};
+            result.defcore_tests.capabilities[cap_class].items[cap] = capability;
+            if (capability.fully_supported) {
+                result.defcore_tests.capabilities[cap_class].full_support_count++
+            }
+            if (capability.particulary_supported){
+                result.defcore_tests.capabilities[cap_class].particular_support_count++
+            }
         }
+        result.defcore_tests.capabilities[cap_class].full_unsupport_count = result.defcore_tests.capabilities[cap_class].count - result.defcore_tests.capabilities[cap_class].particular_support_count
     }
-    return caps_list
+    result.defcore_tests.total_passed_count = test_result.results.length - other_tests.length;
 
+    result.other_tests =  {
+        'list': other_tests,
+        'count': other_tests.length
+    };
+    return result
 }
 
 function render_result(data){
     var caps_list = build_caps_list(data);
     var result = build_result(caps_list);
-    var template = $('#test_result_template').html();
-    var rendered = Mustache.render(template, result);
-    console.log(template);
-    console.log(result);
-    $("div#results").html(rendered);
+    function render_result_callback(result){
+        var stored_result = result;
+        return function(template){
+            console.log(stored_result);
+            var rendered = Mustache.render(template, stored_result);
+            $("div#results").html(rendered);
+        }
+    }
+    $.get('detailed_test_result_template.mst', render_result_callback(result))
 }
 
 function render_result_page(data, status, xhr){
+    render_criteria(data);
     render_result(data);
 }
