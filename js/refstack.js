08771606aef246fcca058260b1a03aeffe926965
sslypushenko <sslypushenko@mirantis.com>
2014-12-23 15:39:18 +0200

    .

--- js/refstack.js
+++ js/refstack.js
@@ -1,3 +1,16 @@
+//
+//   Licensed under the Apache License, Version 2.0 (the "License"); you may
+//   not use this file except in compliance with the License. You may obtain
+//   a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//   License for the specific language governing permissions and limitations
+//   under the License.
+
 /*global $:false */
 /*global Mustache:false */
 /*global window:false */
@@ -261,7 +274,7 @@ var post_processing = function post_processing() {
     $('div.cap_shot:even').addClass('zebra_even');
     $('div#core_filter').buttonset();
     $('div#admin_filter').buttonset();
-    $('#schema_selector').selectmenu({change: function () {window.render_page()} });
+    $('#schema_selector').selectmenu({change: function () {window.render_page(); } });
 
 };
 
